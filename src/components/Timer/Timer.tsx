cca1021e7be991786a2b0c48244f2ad5eab1a34d
navarcus <bspalamutcu@gmail.com>
2022-09-30 04:00:13 +0300

    countdown boilerplate added

--- src/components/Timer/Timer.tsx
+++ src/components/Timer/Timer.tsx
@@ -1,41 +1,61 @@
-import { View, StyleSheet } from 'react-native'
-import React from 'react'
+import { View, StyleSheet, Text } from 'react-native';
+import React from 'react';
 
-const Timer = () => {
+const Timer: React.FC = () => {
+  const defaultTime = 25 * 60; // 25 minutes
+  const [count, setCount] = React.useState(defaultTime);
+
+  const minutes: number = React.useMemo(() => {
+    return Math.floor(count / 60);
+  }, [count]);
+
+  const seconds: number = React.useMemo(() => {
+    return Math.floor(count % 60);
+  }, [count]);
+
+  const countDown = () => {
+    const interval = setInterval(() => {
+      setCount((state) => state - 1);
+    }, 1000);
+  };
+  
+  React.useEffect(() => {
+    countDown();
+  }, []);
+
   return (
-    <>
+    <View>
       <View style={TimerStyles.timerContainer}>
+        <Text>
+          {minutes} : {seconds}
+        </Text>
       </View>
-    </>
-  )
-}
+    </View>
+  );
+};
 
-export default Timer
-
+export default Timer;
 
 const TimerStyles = StyleSheet.create({
-
-    timerContainer: {
-      width: 320,
-      height: 320,
-      borderRadius: 160,
-      justifyContent: 'center',
-      display: 'flex',
-      alignItems: 'center',
-      backgroundColor: '#e0e0e0',
-      borderEndWidth: 3,
-      borderStartWidth: 3,
-      transform: [{ rotate: '45deg' }],
-      borderStartColor: '#fff',
-      borderEndColor: '#949494',
-      elevation: 10,
-      shadowColor: '#000',
-      shadowOffset: {
-        width: 8,
-        height: 8,
-      },
-      shadowOpacity: 0.4,
-      shadowRadius: 11,
+  timerContainer: {
+    width: 320,
+    height: 320,
+    borderRadius: 160,
+    justifyContent: 'center',
+    display: 'flex',
+    alignItems: 'center',
+    backgroundColor: '#e0e0e0',
+    borderEndWidth: 3,
+    borderStartWidth: 3,
+    borderStartColor: '#fff',
+    borderEndColor: '#949494',
+    elevation: 10,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 8,
+      height: 8,
     },
-  });
-  
+    shadowOpacity: 0.4,
+    shadowRadius: 11,
+  },
+});
