44d58138234c9078a7bb94d8b1303d3399b916b8
navarcus <bspalamutcu@gmail.com>
2022-10-01 16:56:06 +0300

    timer component design completed

--- src/components/Timer/Timer.tsx
+++ src/components/Timer/Timer.tsx
@@ -1,9 +1,15 @@
-import { View, StyleSheet, Text } from 'react-native';
 import React from 'react';
+import { View, Text, TouchableHighlight } from 'react-native';
+import { Icon } from '@rneui/base';
+import { Orbitron_500Medium, useFonts } from '@expo-google-fonts/orbitron';
+import { TimerStyles } from './timer.styles';
 
 const Timer: React.FC = () => {
   const defaultTime = 25 * 60; // 25 minutes
   const [count, setCount] = React.useState(defaultTime);
+  const [fontsLoaded, error] = useFonts({
+    Orbitron_500Medium,
+  });
 
   const minutes: number = React.useMemo(() => {
     return Math.floor(count / 60);
@@ -12,23 +18,59 @@ const Timer: React.FC = () => {
   const seconds: number = React.useMemo(() => {
     return Math.floor(count % 60);
   }, [count]);
+  const iconSize = 60;
+  // const countDown = React.useCallback(() => {
+  //   const interval = setInterval(() => {
+  //     setCount((state) => state - 1);
+  //   }, 1000);
+  // }, []);
 
-  const countDown = () => {
-    const interval = setInterval(() => {
-      setCount((state) => state - 1);
-    }, 1000);
-  };
-  
-  React.useEffect(() => {
-    countDown();
-  }, []);
+  // React.useEffect(() => {
+  //   countDown();
+  // }, []);
 
+
+//TODO: STYLE ISIMLERINI DEGISTIR
+//TODO: KOD REFACTORU YAPILACAK
   return (
-    <View>
+    <View style={TimerStyles.mainContainer}>
       <View style={TimerStyles.timerContainer}>
-        <Text>
-          {minutes} : {seconds}
+        <Text style={TimerStyles.timerFont}>
+          {String(minutes).padStart(2, '0')} :{' '}
+          {String(seconds).padStart(2, '0')}
         </Text>
+      </View>
+      <View style={TimerStyles.iconsContainer}>
+        <TouchableHighlight
+          style={TimerStyles.btnContainer}
+          onPress={() => {
+            null;
+          }}
+        >
+          <View style={TimerStyles.playBtn}>
+            <Icon name="pause" color="#fff" size={iconSize - 20} />
+          </View>
+        </TouchableHighlight>
+        <TouchableHighlight
+          style={TimerStyles.btnContainerPlay}
+          onPress={() => {
+            null;
+          }}
+        >
+          <View style={TimerStyles.playBtn}>
+            <Icon name="play-arrow" color="#000000" size={iconSize} />
+          </View>
+        </TouchableHighlight>
+        <TouchableHighlight
+          style={TimerStyles.btnContainer}
+          onPress={() => {
+            null;
+          }}
+        >
+          <View style={TimerStyles.playBtn}>
+            <Icon name="refresh" color="#fff" size={iconSize - 20} />
+          </View>
+        </TouchableHighlight>
       </View>
     </View>
   );
@@ -36,26 +78,3 @@ const Timer: React.FC = () => {
 
 export default Timer;
 
-const TimerStyles = StyleSheet.create({
-  timerContainer: {
-    width: 320,
-    height: 320,
-    borderRadius: 160,
-    justifyContent: 'center',
-    display: 'flex',
-    alignItems: 'center',
-    backgroundColor: '#e0e0e0',
-    borderEndWidth: 3,
-    borderStartWidth: 3,
-    borderStartColor: '#fff',
-    borderEndColor: '#949494',
-    elevation: 10,
-    shadowColor: '#000',
-    shadowOffset: {
-      width: 8,
-      height: 8,
-    },
-    shadowOpacity: 0.4,
-    shadowRadius: 11,
-  },
-});
