f225465ebb7d2fc20823f6cf89b750cedbe055f1
navarcus <bspalamutcu@gmail.com>
2022-10-01 16:56:28 +0300

    background and general layour changed of Home VIew

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,23 +1,30 @@
-import { StyleSheet, View } from 'react-native';
+import { ImageBackground, StyleSheet, View } from 'react-native';
 import React from 'react';
 import Timer from '../components/Timer/Timer';
+import BottomBar from '../components/bottomBar/BottomBar';
+
+const IMG_URL = { uri : 'https://newevolutiondesigns.com/images/freebies/4k-space-ipad-wallpaper-6.jpg'}
+
 const Home: React.FC = () => {
   return (
-    <View style={homePageStyles.container}>
-      <Timer/>
-    </View>
+    <ImageBackground style={homePageStyles.container} source={IMG_URL} resizeMode="cover">
+      <View style={homePageStyles.container}>
+        <Timer />
+        <BottomBar/>
+      </View>
+    </ImageBackground>
   );
 };
 
 export default Home;
 
-
-  const homePageStyles = StyleSheet.create({
-    container: {
-      backgroundColor: '#e0e0e0',
-      alignItems: 'center',
-      justifyContent: 'center',
-      flex: 1,
-      display: 'flex',
-    }
-})
+const homePageStyles = StyleSheet.create({
+  container: {
+    // backgroundColor: '#e0e0e0',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flex: 1,
+    display: 'flex',
+    position: 'relative'
+  },
+});
